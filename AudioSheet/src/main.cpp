3ac16a9e6b56c8221bfd50a9c8f082369746bf61
Marcelo de Oliveira Bittencourt <mcrmaxx@gmail.com>
2019-03-10 18:53:40 -0300

    Modify window settings

--- AudioSheet/src/main.cpp
+++ AudioSheet/src/main.cpp
@@ -1,13 +1,17 @@
 #include "ofMain.h"
 #include "ofApp.h"
 
-//========================================================================
-int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+int main()
+{
+	ofGLFWWindowSettings settings;
+	settings.setGLVersion(3, 3);
+	settings.decorated = true;
+	settings.resizable = true;
 
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp(new ofApp());
+	settings.setSize(1280, 720);
+	auto mainWnd = ofCreateWindow(settings);
+	auto mainApp = std::make_shared<ofApp>();
 
+	ofRunApp(mainWnd, mainApp);
+	ofRunMainLoop();
 }
