dc073b61f6bd6b7312ad9aba343fe8dcaaf943e0
Marcelo de Oliveira Bittencourt <mcrmaxx@gmail.com>
2019-04-13 13:32:18 -0300

    Add event callback feature

--- AudioSheet/src/StreamInput.h
+++ AudioSheet/src/StreamInput.h
@@ -1,8 +1,12 @@
 #pragma once
+#include "StreamEvent.h"
 
 class StreamInput
 {
+protected:
+	StreamEventCb eventCb;
 public:
+	void setEventCallback(StreamEventCb cb){eventCb = cb;}
 	virtual ~StreamInput() = default;
 	virtual void open(int deviceIndex = 0) = 0;
 	virtual void close() = 0;
