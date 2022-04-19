939a273fef514b2b3c6d59c125eb0a235c634cfe
Marcelo de Oliveira Bittencourt <mcrmaxx@gmail.com>
2022-04-19 18:35:31 -0300

    Update clock pattern code

--- AudioSheet/src/ClockPattern.cpp
+++ AudioSheet/src/ClockPattern.cpp
@@ -1,5 +1,6 @@
 #include "ClockPattern.h"
 #include "ofApp.h"
+#include <random>
 
 ClockPattern::ClockPattern(ofBaseApp* baseApp)
 	: baseApp(baseApp)
@@ -11,32 +12,50 @@ ClockPattern::~ClockPattern()
 void ClockPattern::setup()
 {
 	shader.load("shaders/pattern");
+
+	std::default_random_engine gen;
+	gen.seed(std::time(nullptr));
+	std::uniform_int_distribution<uint32_t> dis(0, 255);
+	auto rand = std::bind(dis, gen);
+
+	noiseTexture.allocate(1024, 1024, GL_RGBA, false);
+	std::vector<uint8_t> noiseBuffer(noiseTexture.getWidth()*noiseTexture.getHeight());
+	std::generate(noiseBuffer.begin(), noiseBuffer.end(), rand);
+	noiseTexture.loadData(noiseBuffer.data(), noiseTexture.getWidth(), noiseTexture.getHeight(), GL_RED, GL_UNSIGNED_BYTE);
+
+	m_Tracks = 8;
+	m_Bandguard = 40;
+	m_MarginX = glm::vec2(40);
+	m_MarginY = glm::vec2(40);
 }
 
 void ClockPattern::draw(float x, float y, float w, float h) const
 {
-	ofRectangle rect(x, y, w, h);
+	draw(ofRectangle(x, y, w, h));
+}
 
-	if (plane.getWidth() != w || plane.getHeight() != h)
+void ClockPattern::draw(const ofRectangle & rect) const
+{
+	if (plane.getWidth() != rect.getWidth() || plane.getHeight() != rect.getHeight())
 	{
-		plane.set(w, h);
+		plane.set(rect.getWidth(), rect.getHeight());
 		plane.mapTexCoords(1, 1, 0, 0);
 	}
 
 	if (shader.isLoaded())
 	{
 		shader.begin();
-		shader.setUniform2f("window.pos", glm::vec2(x, y));
-		shader.setUniform2f("window.siz", glm::vec2(w, h));
+		shader.setUniformTexture("channel0.tex", noiseTexture, 0);
+		shader.setUniform2f("window.pos", rect.getPosition());
+		shader.setUniform2f("window.siz", glm::vec2(rect.getWidth(), rect.getHeight()));
+		shader.setUniform1i("page.tracks", m_Tracks);
+		shader.setUniform1i("page.bandguard", m_Bandguard);
+		shader.setUniform2f("page.margin.xrange", m_MarginX);
+		shader.setUniform2f("page.margin.yrange", m_MarginY);
 		plane.setPosition(rect.getCenter());
 		plane.draw();
 		shader.end();
 	}
-}
-
-void ClockPattern::draw(const ofRectangle & rect) const
-{
-	draw(rect.x, rect.y, rect.width, rect.height);
 }
 
 float ClockPattern::getHeight() const
--- AudioSheet/src/ClockPattern.h
+++ AudioSheet/src/ClockPattern.h
@@ -3,6 +3,7 @@
 #include <ofGraphicsBaseTypes.h>
 #include <ofShader.h>
 #include <of3dPrimitives.h>
+#include <ofTexture.h>
 
 class ClockPattern : public ofBaseDraws
 {
@@ -10,6 +11,11 @@ private:
 	ofBaseApp* baseApp;
 	mutable ofShader shader;
 	mutable ofPlanePrimitive plane;
+	mutable ofTexture noiseTexture;
+	int m_Tracks;
+	int m_Bandguard;
+	glm::vec2 m_MarginX;
+	glm::vec2 m_MarginY;
 public:
 	ClockPattern(ofBaseApp* baseApp);
 	~ClockPattern();
